ca0ae35d837f511f7f9b45e0050adb68f1d6e2b1
Katalina <git@kat.cafe>
2018-08-26 07:25:08 -0500

    use math instead of no math

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const {app} = require('electron')
 const {VRWindow} = require('electron-openvr')
+const { Vector3, Matrix3x4 } = require('node-openvr/tools')
 
 let win
 
+app.disableHardwareAcceleration()
+
 const createWindow = () => {
-  win = new VRWindow({width: 1024,
+  win = new VRWindow({
+    width: 1024,
     height: 1024,
     vr: {
       key: 'electronvr.sample',
@@ -12,7 +16,23 @@ const createWindow = () => {
       fps: 45
     }
   })
+  win.overlay.position = { x: 0, y: 0, z: -1 }
   win.loadFile('./app/index.html')
+
+  const pos = new Vector3(0, 0, -1)
+  let t = 0
+  setInterval(() => {
+    const mat = Matrix3x4.fromTransform({
+      T: pos,
+      R: new Vector3(Math.sin(t), Math.cos(t), t)
+    })
+
+    // console.log({d: mat.data})
+
+    t += 0.01
+    // console.log({mat})
+    win.overlay.transformTrackedDeviceRelativeMatrix(0, mat.data)
+  }, 33)
 }
 
 app.on('ready', createWindow)
